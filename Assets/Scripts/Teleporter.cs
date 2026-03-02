0fb72c92b194c36cd7f69a5a24b4892a664c55c2
Beau <66181330+Beauver@users.noreply.github.com>
2026-03-02 08:41:43 +0100

    :sparkles: Working Teleporters

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -5,7 +5,7 @@ public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Transform targetTeleportLocation;
     [SerializeField] private float teleportOffset = 8f;
-    [SerializeField] private float teleportCooldown = 0.3f;
+    [SerializeField] private float teleportCooldown = 0.4f;
 
     private float _lastTeleportTime = -Mathf.Infinity;
 
@@ -18,7 +18,7 @@ public class Teleporter : MonoBehaviour
         if (otherTeleporter != null) 
             otherTeleporter._lastTeleportTime = Time.time;
 
-        other.transform.position = new Vector3(targetTeleportLocation.position.x, other.transform.position.y, other.transform.position.z) 
+        other.transform.position = new Vector3(targetTeleportLocation.position.x, targetTeleportLocation.position.y, other.transform.position.z) 
                                    + (Vector3)(Vector2)targetTeleportLocation.up * 0.2f;
 
         _lastTeleportTime = Time.time;
